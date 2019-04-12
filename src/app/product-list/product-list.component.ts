3d14dd4d784fafd45dbc481a444113c840ccdcaf
udayan saha <udayan.saha@tavant.com>
2019-04-12 17:51:10 +0530

    adding product list component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+
+  constructor(private _producteService: ProductService) { }
+
+  ngOnInit() {
+    this._producteService.getProducts().subscribe(response =>
+    this.products = response);
+  }
+
+}
